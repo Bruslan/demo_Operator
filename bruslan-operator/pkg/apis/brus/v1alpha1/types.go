c77932121f061875965d7af22e70eef455b9bd2b
Bruslan <bruslan@Bruslans-MacBook-Pro.local>
2018-10-19 16:05:40 +0200

    first Push

---
+++ bruslan-operator/pkg/apis/brus/v1alpha1/types.go
@@ -0,0 +1,32 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type BruslanList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []Bruslan `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type Bruslan struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              BruslanSpec   `json:"spec"`
+	Status            BruslanStatus `json:"status,omitempty"`
+}
+
+type BruslanSpec struct {
+	// Fill me
+
+	BruslanDEMO bool `json ",bruslanDemoBool"`
+}
+type BruslanStatus struct {
+	// Fill me
+	Status string `json ",status"`
+}
---
+++ bruslan-operator/pkg/stub/handler.go
@@ -0,0 +1,81 @@
+package stub
+
+import (
+	"github.com/example-inc/bruslan-operator/pkg/apis/brus/v1alpha1"
+
+	"github.com/operator-framework/operator-sdk/pkg/sdk/action"
+	"github.com/operator-framework/operator-sdk/pkg/sdk/handler"
+	"github.com/operator-framework/operator-sdk/pkg/sdk/types"
+	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"fmt"
+	sdk "github.com/operator-framework/operator-sdk/pkg/sdk/action"
+)
+
+func NewHandler() handler.Handler {
+	return &Handler{}
+}
+
+type Handler struct {
+	// Fill me
+}
+
+func (h *Handler) Handle(ctx types.Context, event types.Event) error {
+	switch o := event.Object.(type) {
+	case *v1alpha1.Bruslan:
+
+		err := action.Create(newbusyBoxPod(o))
+		if err != nil && !errors.IsAlreadyExists(err){
+			logrus.Error("Failed to create busybox pod: %v",err)
+			return err
+		}
+		fmt.Printf("the name of this cr is ",o.Name)
+		if o.Spec.BruslanDEMO {
+			o.Status.Status = "We have a true configuration"
+			fmt.Println("unser Boolean ist auf True")
+		} else {
+			o.Status.Status = "WE have a False configuration"
+			fmt.Println("unser Boolean ist auf false")
+		}
+		sdk.Update(o)
+		// fmt.Printf("%+v\n\n", o)
+	}
+	return nil
+}
+
+// newbusyBoxPod demonstrates how to create a busybox pod
+func newbusyBoxPod(cr *v1alpha1.Bruslan) *v1.Pod {
+	labels := map[string]string{
+		"app": "busy-box",
+	}
+	return &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "busy-box",
+			Namespace:    "neuesNamespace",
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(cr, schema.GroupVersionKind{
+					Group:   v1alpha1.SchemeGroupVersion.Group,
+					Version: v1alpha1.SchemeGroupVersion.Version,
+					Kind:    "Bruslan",
+				}),
+			},
+			Labels: labels,
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:    "busybox",
+					Image:   "busybox",
+					Command: []string{"sleep", "3600"},
+				},
+			},
+		},
+	}
+}
